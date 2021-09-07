82b7ab504ef68dd68cb755209e9aabae76f2748a
Lik-ABC <lorik@DESKTOP-71QNDUD>
2021-09-07 14:49:07 +0200

    Add project files.

---
+++ MediorTest/LocationDataSetHandler/LocationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediorTest.LocationDataSetHandler
+{
+    public class LocationModel
+    {
+        public string LocationName { get; set; }
+        public string Address { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+}
