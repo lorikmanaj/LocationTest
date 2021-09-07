976ff96750462f146400d911a852048d5957bda4
Lik-ABC <lorik@DESKTOP-71QNDUD>
2021-09-07 15:33:02 +0200

    Added implementation for Generic Service

---
+++ MediorTest/Implentation/LocationService.cs
@@ -0,0 +1,32 @@
+﻿using MediorTest.LocationDataSetHandler;
+using MediorTest.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediorTest.Implentation
+{
+    public class LocationService : ILocationService
+    {
+        public LocationModel Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public LocationModel Insert()
+        {
+            throw new NotImplementedException();
+        }
+
+        public LocationModel Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public LocationModel Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MediorTest/Services/ILocationService.cs
@@ -0,0 +1,12 @@
+﻿using MediorTest.LocationDataSetHandler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediorTest.Services
+{
+    public interface ILocationService : IGeneralService<LocationModel>
+    {
+    }
+}
